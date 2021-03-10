9c6407368f1f83c76ff16623b48c095ce8507c4a
musaabdullah <musaabdalwahed@gmail.com>
2021-03-10 12:17:58 +0200

    list view

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import './app.css';
 function App() {
 
     const   {Data}  = usefetch("https://jsonplaceholder.typicode.com/todos");
-    
+    const [show, setShow] = useState(5);   
+
+    const showLess  = () => {
+        if(show === 5) return ; 
+        setShow(show => show - 5)
+    }
     return (
         <>
           {Data && <div className="app">
     
            <div className="container">
                <input type="text" className="container__input" placeholder="Enter todo"/> 
-          {Data.slice(0, 5).map((user) => (
+          {Data.slice(0, show).map((user) => (
               <div key={user.id} className="todo">
                   <div className="todo__start">
                   <div className="todo__id">{user.id}.</div>
                   <div className="todo__title">{user.title}</div>
                   </div>
                   <div className="todo__complation">{user.completed ? "Completed": "Not completed"}</div>
-                  
                   </div>
-              
               ))}  
               <div className="btn">
-                  <button className="btn__more">Show more</button>
-                  <button className="btn__less">Show less</button>
+                  <button onClick={() => setShow(show => show + 5)} className="btn__more">Show more</button>
+                  <button onClick={() =>  showLess() } className="btn__less">Show less</button>
               </div>
               </div>
          </div> }
